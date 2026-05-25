3700668b3b3c06068126e0877ca4513ac56abc3a
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-05-26 07:25:54 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #pacman -Syu --noconfirm base-devel sudo schedtool
-pacman -Syu --noconfirm base-devel sudo git curl unzip schedtool
+pacman -Syu --noconfirm base-devel sudo git schedtool
 pacman --noconfirm -U llvm-git*.pkg.tar.zst llvm-libs-git*.pkg.tar.zst
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --keyserver hkps://keys.openpgp.org --recv-key 19802F8B0D70FC30"
