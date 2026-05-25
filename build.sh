eb500417d535856f414d6ec8dbf91310c7b1c5ee
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-05-26 07:22:32 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 #pacman -Syu --noconfirm base-devel sudo schedtool
-pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool
-curl -sLJO -H 'Accept: application/octet-stream' \
-"https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/releases/assets/$( \
-curl -sL https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/releases/tags/test \
-| jq '.assets[] | select(.name | contains("llvm")) | .id')" -o llvm-git.zip
-unzip llvm-git.zip
+pacman -Syu --noconfirm base-devel sudo git curl unzip schedtool
 pacman --noconfirm -U llvm-git*.pkg.tar.zst llvm-libs-git*.pkg.tar.zst
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --keyserver hkps://keys.openpgp.org --recv-key 19802F8B0D70FC30"
