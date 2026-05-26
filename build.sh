935a3a34c5e270ea21af5eb7f873f03ebd4625e1
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-05-26 09:59:14 +0900

    Change GPG keyserver to ubuntu.com in build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ USERID=$1
 pacman -Syu --noconfirm base-devel sudo git schedtool
 pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-su builder -c "gpg --keyserver hkps://keys.openpgp.org --recv-key 19802F8B0D70FC30"
-su builder -c "gpg --keyserver hkps://keys.openpgp.org --recv-key 38DBBDC86092693E"
-su builder -c "gpg --keyserver hkps://keys.openpgp.org --recv-key B8AC08600F108CDF"
+su builder -c "gpg --keyserver hkps://ubuntu.com --recv-key 19802F8B0D70FC30"
+su builder -c "gpg --keyserver hkps://ubuntu.com --recv-key 38DBBDC86092693E"
+su builder -c "gpg --keyserver hkps://ubuntu.com --recv-key B8AC08600F108CDF"
 cd ./linux-zen ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm -sc"
