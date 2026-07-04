4568a0117acbfe3b3f154470e4c59967ecfe463e
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-07-05 08:15:37 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 USERID=$1
 
 #pacman -Syu --noconfirm base-devel sudo schedtool
-pacman -Syu --noconfirm base-devel sudo git schedtool pacman-contrib
+pacman -Syu --noconfirm base-devel sudo git schedtool
 pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 sed -i \
   's|^  https://cdn\.kernel\.org/pub/linux/kernel/v${pkgver%%\.\*}\.x/${_srcname}\.tar\.{xz,sign}$|  "$_srcname::git+https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git?signed#tag=v${pkgver%.*}"|' \
@@ -12,8 +12,8 @@ grep -oP "(?<=')[a-f0-9]{64}(?=')" ./linux-zen/PKGBUILD | awk '{print "sed -i \x
 grep -oP "(?<=')[a-f0-9]{128}(?=')" ./linux-zen/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 ./linux-zen/PKGBUILD"}' | bash
 #sed -i "s/37198c93727be247c9fb5309bb86cd5e496c61e5322cd8c4eca9476bb0b5883f/SKIP/g" ./linux-zen/PKGBUILD
 #sed -i "s/0d6e9ff535af085190da7df50887b20f395cd4d6befb7158c9993bf77fe92459a9982877ce944ca522192daa5a54c952c3d368def04b579796ba7109a972453b/SKIP/g" ./linux-zen/PKGBUILD
-updpkgsums ./linux-zen/PKGBUILD
-cat ./linux-zen/PKGBUILD
+#updpkgsums ./linux-zen/PKGBUILD
+#cat ./linux-zen/PKGBUILD
 
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --recv-keys B8AC08600F108CDF"
