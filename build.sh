53b559e78914b360c23d1161ac402706acd16f8a
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-07-04 14:48:38 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 USERID=$1
 
 #pacman -Syu --noconfirm base-devel sudo schedtool
-pacman -Syu --noconfirm base-devel sudo git schedtool
+pacman -Syu --noconfirm base-devel sudo git schedtool pacman-contrib
 pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
+updpkgsums PKGBUILD
+
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --recv-keys B8AC08600F108CDF"
 su builder -c "gpg --recv-keys 38DBBDC86092693E"
