2e36c6ff94895185bbd81e82726e032cb31e73e6
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-07-04 15:16:16 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ sed -i \
   's|^  https://cdn\.kernel\.org/pub/linux/kernel/v\$\{pkgver%%\.\*\}\.x/\$\{_srcname\}\.tar\.\{xz,sign\}$|  "$_srcname::git+https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git?signed#tag=v${pkgver%.*}"|' \
   ./linux-zen/PKGBUILD
 updpkgsums ./linux-zen/PKGBUILD
+cat ./linux-zen/PKGBUILD
 
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --recv-keys B8AC08600F108CDF"
