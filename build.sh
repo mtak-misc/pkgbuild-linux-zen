103397622b2b7ad662641567f545570edd0f8ec1
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-09-15 23:09:06 +0900

    Remove PATH variable from makepkg command

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --recv-keys B8AC08600F108CDF"
 su builder -c "gpg --recv-keys 38DBBDC86092693E"
-cd ./linux-zen ; su builder -c "yes '' | PATH=/usr/lib/llvm21/bin:$PATH MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm -sc"
+cd ./linux-zen ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm -sc"
