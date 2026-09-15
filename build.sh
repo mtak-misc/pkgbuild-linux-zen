8966903a656affe681acaa707a9a8285218bed90
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-09-15 21:28:04 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ USERID=$1
 #pacman -Syu --noconfirm base-devel sudo schedtool
 pacman -Syu --noconfirm base-devel sudo git schedtool
 #pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
-pacman -S --noconfirm clang21 lld21
+pacman -S --noconfirm clang21 lld21 llvm21
 
 #sed -i \
 #  's|^  https://cdn\.kernel\.org/pub/linux/kernel/v${pkgver%%\.\*}\.x/${_srcname}\.tar\.{xz,sign}$|  "$_srcname::git+https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git?signed#tag=v${pkgver%.*}"|' \
